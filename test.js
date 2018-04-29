847cf0153010711c8c7d184af60106a25a2734ce
Ben Pardo <visaudi@gmail.com>
2018-04-29 00:28:49 -0600

    Tests personal greeting.

--- test.js
+++ test.js
@@ -5,4 +5,5 @@ const assert = require("assert").strictEqual;
 const makeGreeting = require("./index");
 
 assert(makeGreeting(), "Hello world!");
+assert(makeGreeting("Zach"), "Hello Zach!");
 console.log("Success");
